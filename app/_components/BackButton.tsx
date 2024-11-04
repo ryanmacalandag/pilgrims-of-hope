c4dda0be3efd3ddfd3629fa2b3e21d9102a53560
Ryan Macalandag <rmacalandag@gmail.com>
2024-11-05 10:43:59 +1100

    resize back-button

--- app/_components/BackButton.tsx
+++ app/_components/BackButton.tsx
@@ -6,9 +6,9 @@ export const BackButton = () => {
   
   return (
     <div className='pl-8'>
-      <div className='group relative w-full h-8 border-b-4 border-dotted border-orange-400/60 hover:border-stone-800/40 mb-2'>
+      <div className='group relative w-full h-8 border-b-4 border-dotted border-orange-400/60 hover:border-stone-800/40 mb-8'>
         <Link href='/sites' aria-label="Go back a page">
-          <BiLeftArrowAlt size={48} className='absolute bottom-0 left-0 translate-y-1/2 text-white bg-orange-500 group-hover:bg-stone-800 group-hover:-translate-x-2 rounded-full p-1 transition duration-300'></BiLeftArrowAlt>
+          <BiLeftArrowAlt size={38} className='absolute bottom-0 left-0 translate-y-1/2 text-white bg-orange-500 group-hover:bg-stone-800 group-hover:-translate-x-2 rounded-full p-1 transition duration-300'></BiLeftArrowAlt>
         </Link>
       </div>
     </div>
