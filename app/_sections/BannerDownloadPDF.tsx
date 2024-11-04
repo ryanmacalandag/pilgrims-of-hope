05e6c688b2d4023eb6082cc3b3fe1e281584ad24
Ryan Macalandag <rmacalandag@gmail.com>
2024-11-05 10:29:18 +1100

    deleted repeating data

--- app/_sections/BannerDownloadPDF.tsx
+++ app/_sections/BannerDownloadPDF.tsx
@@ -6,7 +6,7 @@ export default function BannerDownloadPDF() {
   return (
     <section>
       <div className="group relative w-full max-w-screen-lg mx-auto px-6">
-        <div className='flex md:flex-row flex-col justify-between items-start md:items-center gap-10 text-white bg-gradient-to-bl from-stone-800 via-stone-800 to-stone-600 px-10 sm:px-12 py-16 rounded-2xl my-8 lg:my-12'>
+        <div className='w-4/5 flex md:flex-row flex-col justify-between items-start md:items-center gap-10 text-white bg-gradient-to-bl from-stone-800 via-stone-800 to-stone-600 px-10 sm:px-12 py-16 rounded-2xl my-8 lg:my-12'>
           <div className='flex flex-col gap-3'>
             <div className='flex gap-2 items-center text-2xl md:text-3xl font-serif text-balance'>Download the PDF version</div>
             <p className='text-balance'>Need a printed version of the list? We compiled all the pilgrimage sites into a convenient PDF book you can print at home.</p>
