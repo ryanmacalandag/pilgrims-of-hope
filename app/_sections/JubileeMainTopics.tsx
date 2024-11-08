6bcef7f224a287f212bfdf4e2a0585b417431303
Ryan Macalandag <rmacalandag@gmail.com>
2024-11-08 16:20:57 +1100

    fix padding

--- app/_sections/JubileeMainTopics.tsx
+++ app/_sections/JubileeMainTopics.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 export default function JubileeMainTopics() {
   return (
     <div className='grid grid-cols-12'>
-      <Link href="https://www.iubilaeum2025.va/en.html" target="_blank" className='relative col-span-12 md:col-span-4 aspect-video sm:aspect-banner md:aspect-square lg:aspect-video px-8 py-8 bg-green-800 text-white overflow-hidden hover:brightness-150 transition duration-300'>
+      <Link href="https://www.iubilaeum2025.va/en.html" target="_blank" className='relative col-span-12 md:col-span-4 aspect-video sm:aspect-banner md:aspect-square lg:aspect-video bg-green-800 text-white overflow-hidden hover:brightness-150 transition duration-300'>
           <div className='group w-full h-full flex flex-row justify-start items-end gap-2'>
-            <h3 className='z-10 text-2xl md:text-2xl font-serif font-bold'>What is Jubilee 2025?</h3>
+            <h3 className='absolute bottom-0 left-0 w-full items-end z-10 text-2xl md:text-2xl font-serif font-bold px-8 py-8'>What is Jubilee 2025?</h3>
             <Image
               src='/jubilee/jubilee-main-topic-1.jpg'
               alt='main topic 1'
@@ -20,7 +20,7 @@ export default function JubileeMainTopics() {
       </Link>
       <Link href="https://www.iubilaeum2025.va/en/giubileo-2025/bolla.html" target="_blank" className='relative col-span-12 md:col-span-4 aspect-video sm:aspect-banner md:aspect-square lg:aspect-video px-8 py-8 bg-green-800 text-white overflow-hidden hover:brightness-150 transition duration-300'>
           <div className='group w-full h-full flex flex-row justify-start items-end gap-2'>
-            <h3 className='z-10 text-2xl md:text-2xl font-serif font-bold'>Read the Papal Bull</h3>
+            <h3 className='absolute bottom-0 left-0 w-full items-end z-10 text-2xl md:text-2xl font-serif font-bold px-8 py-8'>Read the Papal Bull</h3>
             <Image
               src='/jubilee/jubilee-main-topic-2.jpg'
               alt='main topic 1'
@@ -33,7 +33,7 @@ export default function JubileeMainTopics() {
       </Link>
       <Link href="https://www.iubilaeum2025.va/en/calendario/calendario-generale.html" target="_blank" className='relative col-span-12 md:col-span-4 aspect-video sm:aspect-banner md:aspect-square lg:aspect-video px-8 py-8 bg-green-800 text-white overflow-hidden hover:brightness-150 transition duration-300'>
           <div className='group w-full h-full flex flex-row justify-start items-end gap-2'>
-            <h3 className='z-10 text-2xl md:text-2xl font-serif font-bold'>2025 Jubilee Calendar</h3>
+            <h3 className='absolute bottom-0 left-0 w-full items-end z-10 text-2xl md:text-2xl font-serif font-bold px-8 py-8'>2025 Jubilee Calendar</h3>
             <Image
               src='/jubilee/jubilee-main-topic-3.jpg'
               alt='main topic 1'
