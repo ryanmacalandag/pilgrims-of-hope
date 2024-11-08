5f530f8400a24b36ebe8dd76786d4a80817f136e
Ryan Macalandag <rmacalandag@gmail.com>
2024-11-08 21:10:29 +1100

    animated max-h

--- app/_sections/Jubilee2025Primer.tsx
+++ app/_sections/Jubilee2025Primer.tsx
@@ -7,7 +7,7 @@ export default function Jubilee2025Primer() {
   return (
     <section className='my-8 lg:my-12'>
       <div className='w-full max-w-screen-xl mx-auto px-0 sm:px-12'>
-        <div className='w-full lg:gap-12 pt-12 lg:pt-20 pb-16 lg:pb-20 bg-white rounded-none sm:rounded-2xl'>
+        <div className='w-full lg:gap-12 pt-12 lg:pt-20 pb-4 lg:pb-8 bg-white rounded-none sm:rounded-2xl'>
           <div className='flex flex-col gap-8 border-b-2 border-green-500 border-dotted pb-2 px-6 md:px-12 lg:px-16'>
             <h2 className='text-2xl md:text-3xl font-serif tracking-tight'>Jubilee Year 2025 Primer</h2>
           </div>
--- app/_sections/JubileeFAQAccordion.tsx
+++ app/_sections/JubileeFAQAccordion.tsx
@@ -29,57 +29,57 @@ export default function JubileeFAQAccordion() {
         <div className='col-span-12 md:col-span-7'>
           <div className='grid grid-cols-12 gap-4'>
             <div className='col-span-12'>
-              <Checkbox className='group flex flex-col gap-2 lg:gap-4 border-b border-stone-300'>
+              <Checkbox className='group flex flex-col gap-2 border-b border-stone-300'>
                 <div className='flex flex-shrink-0 gap-2 items-center'>
                   <BiChevronRight size={24} className='w-fit min-w-6 rotate-0 group-aria-[checked=true]:rotate-90 transition duration-300'></BiChevronRight>
                   <h2 className='text-lg *:md:text-xl font-serif font-bold '>What is the Jubilee 2025, and why is it important?</h2>
                 </div>
-                <p className='h-full max-h-0 group-aria-[checked=true]:max-h-80 overflow-hidden transition duration-300 mb-4 text-base md:text-base pl-8 '>The Jubilee 2025, also called the Holy Year, is a special year of grace and celebration within the Catholic Church, set to unite believers around the world in faith, renewal, and reconciliation. Marked by Pope Francis as a year of hope, this Jubilee invites the faithful to reflect on God&#39;s mercy and to seek spiritual growth through acts of devotion, prayer, and charity.</p>
+                <p className='max-h-0 group-aria-[checked=true]:max-h-[500] overflow-hidden transition-max-h duration-500 delay-75 mb-4 text-base md:text-base pl-8 '>The Jubilee 2025, also called the Holy Year, is a special year of grace and celebration within the Catholic Church, set to unite believers around the world in faith, renewal, and reconciliation. Marked by Pope Francis as a year of hope, this Jubilee invites the faithful to reflect on God&#39;s mercy and to seek spiritual growth through acts of devotion, prayer, and charity.</p>
               </Checkbox>
             </div>
             <div className='col-span-12'>
-              <Checkbox className='group flex flex-col gap-2 lg:gap-4 border-b border-stone-300'>
+              <Checkbox className='group flex flex-col gap-2 border-b border-stone-300'>
                 <div className='flex flex-shrink-0 gap-2 items-center'>
                   <BiChevronRight size={24} className='w-fit min-w-6 rotate-0 group-aria-[checked=true]:rotate-90 transition duration-300'></BiChevronRight>
                   <h2 className='text-lg *:md:text-xl font-serif font-bold '>When does the Jubilee 2025 begin, and how long does it last?</h2>
                 </div>
-                <p className='h-full max-h-0 group-aria-[checked=true]:max-h-80 overflow-hidden transition duration-300 mb-4 text-base md:text-base pl-8'>The Jubilee 2025 officially begins on December 8, 2024, with the opening of the Holy Door at St. Peter&#39;s Basilica in Rome. It will run until November 2025, culminating in various celebrations, pilgrimages, and ceremonies dedicated to fostering unity, compassion, and faith.</p>
+                <p className='max-h-0 group-aria-[checked=true]:max-h-[500] overflow-hidden transition-max-h duration-500 delay-75 mb-4 text-base md:text-base pl-8'>The Jubilee 2025 officially begins on December 8, 2024, with the opening of the Holy Door at St. Peter&#39;s Basilica in Rome. It will run until November 2025, culminating in various celebrations, pilgrimages, and ceremonies dedicated to fostering unity, compassion, and faith.</p>
               </Checkbox>
             </div>
             <div className='col-span-12'>
-              <Checkbox className='group flex flex-col gap-2 lg:gap-4 border-b border-stone-300'>
+              <Checkbox className='group flex flex-col gap-2 border-b border-stone-300'>
                 <div className='flex flex-shrink-0 gap-2 items-center'>
                   <BiChevronRight size={24} className='w-fit min-w-6 rotate-0 group-aria-[checked=true]:rotate-90 transition duration-300'></BiChevronRight>
                   <h2 className='text-lg *:md:text-xl font-serif font-bold '>What are the main themes of Jubilee 2025?</h2>
                 </div>
-                <p className='h-full max-h-0 group-aria-[checked=true]:max-h-80 overflow-hidden transition duration-300 mb-4 text-base md:text-base pl-8 '>Pope Francis has designated the theme of Jubilee 2025 as Pilgrims of Hope. This theme encourages Catholics worldwide to journey spiritually towards hope, drawing strength from faith to overcome life&#39;s challenges. Emphasizing reconciliation and mercy, the Jubilee reminds the faithful of God&#39;s unwavering love and the Church&#39;s call to spread hope.</p>
+                <p className='max-h-0 group-aria-[checked=true]:max-h-[500] overflow-hidden transition-max-h duration-500 delay-75 mb-4 text-base md:text-base pl-8 '>Pope Francis has designated the theme of Jubilee 2025 as Pilgrims of Hope. This theme encourages Catholics worldwide to journey spiritually towards hope, drawing strength from faith to overcome life&#39;s challenges. Emphasizing reconciliation and mercy, the Jubilee reminds the faithful of God&#39;s unwavering love and the Church&#39;s call to spread hope.</p>
               </Checkbox>
             </div>
             <div className='col-span-12'>
-              <Checkbox className='group flex flex-col gap-2 lg:gap-4 border-b border-stone-300'>
+              <Checkbox className='group flex flex-col gap-2 border-b border-stone-300'>
                 <div className='flex flex-shrink-0 gap-2 items-center'>
                   <BiChevronRight size={24} className='w-fit min-w-6 rotate-0 group-aria-[checked=true]:rotate-90 transition duration-300'></BiChevronRight>
                   <h2 className='text-lg *:md:text-xl font-serif font-bold '> What is the significance of the Holy Door, and how can I participate in this tradition?</h2>
                 </div>
-                <p className='h-full max-h-0 group-aria-[checked=true]:max-h-80 overflow-hidden transition duration-300 mb-4 text-base md:text-base pl-8 '>The Holy Door is a sacred symbol of Christ, who opens the way to salvation. During the Jubilee, Holy Doors at major basilicas in Rome and other designated churches worldwide will open as a gesture of spiritual welcome. Pilgrims who pass through the Holy Door, in a spirit of penance and devotion, are invited to receive a special indulgence. You can participate by visiting any designated Holy Door location and fulfilling the sacramental conditions for indulgences.</p>
+                <p className='max-h-0 group-aria-[checked=true]:max-h-[500] overflow-hidden transition-max-h duration-500 delay-75 mb-4 text-base md:text-base pl-8 '>The Holy Door is a sacred symbol of Christ, who opens the way to salvation. During the Jubilee, Holy Doors at major basilicas in Rome and other designated churches worldwide will open as a gesture of spiritual welcome. Pilgrims who pass through the Holy Door, in a spirit of penance and devotion, are invited to receive a special indulgence. You can participate by visiting any designated Holy Door location and fulfilling the sacramental conditions for indulgences.</p>
               </Checkbox>
             </div>
             <div className='col-span-12'>
-              <Checkbox className='group flex flex-col gap-2 lg:gap-4 border-b border-stone-300'>
+              <Checkbox className='group flex flex-col gap-2 border-b border-stone-300'>
                 <div className='flex flex-shrink-0 gap-2 items-center'>
                   <BiChevronRight size={24} className='w-fit min-w-6 rotate-0 group-aria-[checked=true]:rotate-90 transition duration-300'></BiChevronRight>
                   <h2 className='text-lg *:md:text-xl font-serif font-bold '>What are indulgences, and how can one obtain an indulgence during the Jubilee?</h2>
                 </div>
-                <p className='h-full max-h-0 group-aria-[checked=true]:max-h-80 overflow-hidden transition duration-300 mb-4 text-base md:text-base pl-8 '>An indulgence is a spiritual grace that lessens the temporal punishment due to sins. During the Jubilee, the Church offers special indulgences to encourage reconciliation and personal conversion. To obtain an indulgence, pilgrims are called to pass through the Holy Door with a contrite heart, participate in the Eucharist, pray for the Pope&#39;s intentions, and perform acts of charity or mercy.</p>
+                <p className='max-h-0 group-aria-[checked=true]:max-h-[500] overflow-hidden transition-max-h duration-500 delay-75 mb-4 text-base md:text-base pl-8 '>An indulgence is a spiritual grace that lessens the temporal punishment due to sins. During the Jubilee, the Church offers special indulgences to encourage reconciliation and personal conversion. To obtain an indulgence, pilgrims are called to pass through the Holy Door with a contrite heart, participate in the Eucharist, pray for the Pope&#39;s intentions, and perform acts of charity or mercy.</p>
               </Checkbox>
             </div>
             <div className='col-span-12'>
-              <Checkbox className='group flex flex-col gap-2 lg:gap-4 border-b border-stone-300'>
+              <Checkbox className='group flex flex-col gap-2 border-b border-stone-300'>
                 <div className='flex flex-shrink-0 gap-2 items-center'>
                   <BiChevronRight size={24} className='w-fit min-w-6 rotate-0 group-aria-[checked=true]:rotate-90 transition duration-300'></BiChevronRight>
                   <h2 className='text-lg *:md:text-xl font-serif font-bold '>How can Catholics in Australia participate in the Jubilee 2025 if they cannot travel to Rome?</h2>
                 </div>
-                <p className='h-full max-h-0 group-aria-[checked=true]:max-h-80 overflow-hidden transition duration-300 mb-4 text-base md:text-base pl-8 '>Catholics in Australia can celebrate the Jubilee through local pilgrimages to designated Holy Door churches, special liturgies, and events hosted by dioceses and parishes. In addition, prayer and acts of charity are encouraged as ways to embody the Jubilee&#39;s spirit of hope and unity. The Australian Catholic Bishops Conference will provide resources and information on local events and digital tools to help Australian Catholics participate fully in the Jubilee experience from afar.</p>
+                <p className='max-h-0 group-aria-[checked=true]:max-h-[500] overflow-hidden transition-max-h duration-500 delay-75 mb-4 text-base md:text-base pl-8 '>Catholics in Australia can celebrate the Jubilee through local pilgrimages to designated Holy Door churches, special liturgies, and events hosted by dioceses and parishes. In addition, prayer and acts of charity are encouraged as ways to embody the Jubilee&#39;s spirit of hope and unity. The Australian Catholic Bishops Conference will provide resources and information on local events and digital tools to help Australian Catholics participate fully in the Jubilee experience from afar.</p>
               </Checkbox>
             </div>
           </div>
