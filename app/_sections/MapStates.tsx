51e332914621a43490e3ff7d9abb3d36e9167a68
Ryan Macalandag <rmacalandag@gmail.com>
2024-12-02 17:07:04 +1100

    minor maps

--- app/_sections/MapStates.tsx
+++ app/_sections/MapStates.tsx
@@ -7,7 +7,7 @@ export default function MapsState({ lat, lng }: { lat: number; lng: number }) {
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_EMBED_API_KEY!}>
       <Map
-        className="w-full h-[400px]"
+        className="w-full h-[200px] md:h-[280px]"
         zoom={5}
         center={{ lat, lng }}
         gestureHandling={"greedy"}
--- app/_sections/StatesHeaderMap.tsx
+++ app/_sections/StatesHeaderMap.tsx
@@ -15,9 +15,10 @@ export default function StatesHeaderMap({
       <div className="absolute z-30 top-0 right-0 md:right-1/2 md-hover:h-20 translate-x-0 md:translate-x-1/2 w-fit px-[27px] py-3 text-orange-100/80 group-hover:text-white rounded-bl-xl md:rounded-br-xl bg-stone-950 group-hover:bg-orange-600/90 transition duration-300">
         <BiMapAlt></BiMapAlt>
       </div>
-      <div className="w-full h-fit max-h-[400px] group-aria-[checked=true]:max-h-[100px] overflow-hidden transition-max-h duration-500 ease-in-out">
+      <div className="w-full h-fit max-h-[800px] group-aria-[checked=true]:max-h-[10px] overflow-hidden transition-max-h duration-500 ease-in-out">
         <MapsState lat={lat} lng={lng}></MapsState>
       </div>
+      <div className="absolute bottom-0 left-0 w-full h-1/3 translate-y-1/2 bg-gradient-to-t from-transparent via-white to-transparent"></div>
     </Checkbox>
   );
 }
